75a40be4c3848428bfa946b2474b183c99c87c1e
morgan-levetti <morgan.levetti@laplateforme.io>
2022-01-05 14:33:12 +0100

    login role_user(5-01) et route update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,14 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+  
 class SecurityController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_ADMIN")
      * @Route("/login", name="login")
      */
     public function index(): Response
     {
+        
         return $this->render('security/index.html.twig', [
             'controller_name' => 'SecurityController',
         ]);
@@ -26,7 +33,7 @@ class SecurityController extends AbstractController
 
     // last username entered by the user
     $lastUsername = $authenticationUtils->getLastUsername();
-
+    // view formulaire login
     return $this->render('security/index.html.twig', [
         'last_username' => $lastUsername,
         'error'         => $error,
