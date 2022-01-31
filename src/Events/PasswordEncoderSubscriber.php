1c4ae26419fff9e14b08a6f9f5da8dd09f180136
swattiez <swattiez@fr.scc.com>
2022-01-31 15:21:18 +0100

    add jwt

---
+++ src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+ 
+namespace App\Events;
+ 
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+ 
+class PasswordEncoderSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $hashPassword;
+ 
+    public function __construct(UserPasswordHasherInterface $hashPassword)
+    {
+        $this->hashPassword = $hashPassword;
+    }
+ 
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+ 
+    public function encodePassword(ViewEvent $event)
+    {
+        $result = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+ 
+        if ($result instanceof User && $method === "POST") {
+            $hash = $this->hashPassword->hashPassword($result, $result->getPassword());
+            $result->setPassword($hash);
+        }
+    }
+}
